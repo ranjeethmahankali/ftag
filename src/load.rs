bb066038e6b525f8bd4f80cc4a66a4960328ae03
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-01-19 22:24:33 -0500

    Simplify with a saturating sub

--- src/load.rs
+++ src/load.rs
@@ -58,13 +58,7 @@ fn infer_format_tag(input: &str) -> impl Iterator<Item = String> + '_ {
     EXT_TAG_MAP.iter().filter_map(|(exts, tag)| {
         if exts
             .iter()
-            .any(|ext| {
-                if input.len() > ext.len() {
-                    input[input.len() - ext.len()..].eq_ignore_ascii_case(ext)
-                } else {
-                    false
-                }
-            })
+            .any(|ext| input[input.len().saturating_sub(ext.len())..].eq_ignore_ascii_case(ext))
         {
             Some(tag.to_string())
         } else {
