39cadbbbaad8766c0cf86ab871c2225aef832ad4
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-09-01 21:40:44 -0400

    Have something kinda working

--- src/tui.rs
+++ src/tui.rs
@@ -13,7 +13,7 @@ use crossterm::{
         enable_raw_mode,
     },
 };
-use std::fmt::Write;
+use std::fmt::Write as FmtWrite;
 
 /// Count digits in the integer as written in base 10.
 fn count_digits(mut num: usize) -> u8 {
@@ -104,9 +104,9 @@ impl TuiApp {
         if end > ntags {
             self.scroll = self.scroll.saturating_sub(end - ntags);
         }
-        // 3 rows for the header, 4 rows for the footer -> total 7 rows.
+        // 2 rows for the header, 4 rows for the footer -> total 7 rows.
         let old_files_per_page = self.files_per_page;
-        self.files_per_page = h.saturating_sub(7);
+        self.files_per_page = h.saturating_sub(6);
         (self.num_pages, self.page_index) = if self.files_per_page == 0 {
             (0, 0)
         } else {
@@ -170,20 +170,6 @@ impl TuiApp {
         }
     }
 
-    fn render_tag(buf: &mut String, tag: Option<&String>, width: usize) -> std::fmt::Result {
-        match tag {
-            Some(tag) => {
-                let end = width.min(tag.len());
-                write!(buf, "{}", &tag[..end])?;
-                buf.extend(std::iter::repeat(' ').take(width - end));
-            }
-            None => {
-                buf.extend(std::iter::repeat(' ').take(width));
-            }
-        }
-        Ok(())
-    }
-
     fn render(&mut self, stdout: &mut std::io::Stdout) -> Result<(), TuiError> {
         let (ncols, nrows) = crossterm::terminal::size()?;
         self.set_frame_height(nrows as usize);
@@ -198,33 +184,32 @@ impl TuiApp {
             .skip(self.scroll)
             .take(nrows as usize);
 
-        // Render first line with the top bar.
-        Self::render_tag(&mut self.screen_buf, tags.next(), lwidth)?;
-        self.screen_buf
-            .extend(std::iter::once('├').chain(std::iter::repeat('─').take(rwidth)));
-        // Render second line with heading.
-        Self::render_tag(&mut self.screen_buf, tags.next(), lwidth)?;
-        self.screen_buf.push('│');
-        let target_len = self.screen_buf.len() + rwidth;
+        // Render first line with the top bar heading.
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}│")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}│", "")?,
+        }
         write!(
             self.screen_buf,
-            "{}: {} results, page {} of {}",
-            if self.session.filter_str().is_empty() {
-                "ALL_TAGS"
-            } else {
-                self.session.filter_str()
-            },
-            self.session.filelist().len(),
-            self.page_index + 1,
-            self.num_pages
+            "{:^rwidth$.rwidth$}",
+            format!(
+                "{}: {} results, page {} of {}",
+                if self.session.filter_str().is_empty() {
+                    "ALL_TAGS"
+                } else {
+                    self.session.filter_str()
+                },
+                self.session.filelist().len(),
+                self.page_index + 1,
+                self.num_pages
+            )
         )?;
-        self.screen_buf.truncate(target_len);
-        let nspaces = target_len - self.screen_buf.len();
-        self.screen_buf.extend(std::iter::repeat(' ').take(nspaces));
-        // Render third line with the border.
-        Self::render_tag(&mut self.screen_buf, tags.next(), lwidth)?;
-        self.screen_buf
-            .extend(std::iter::once('├').chain(std::iter::repeat('─').take(rwidth)));
+        // Render second line with the border.
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}├")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}├", "")?,
+        }
+        write!(self.screen_buf, "{:─<rwidth$.rwidth$}", "")?;
         // Render the lines corresponding to file paths.
         let mut files = self
             .session
@@ -235,30 +220,52 @@ impl TuiApp {
             .take(self.files_per_page);
         let mut prevfile: &str = "";
         for _ in 0..self.files_per_page {
-            let target_len = self.screen_buf.len() + (ncols as usize);
-            Self::render_tag(&mut self.screen_buf, tags.next(), lwidth)?;
-            self.screen_buf.push_str("│ ");
+            match tags.next() {
+                Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}│")?,
+                None => write!(self.screen_buf, "{:<lwidth$.lwidth$}│", "")?,
+            }
             match files.next() {
                 Some((i, file)) => {
-                    self.screen_buf.push('[');
-                    let nspaces = self.file_index_width.saturating_sub(count_digits(i));
-                    self.screen_buf
-                        .extend(std::iter::repeat(' ').take(nspaces as usize));
-                    write!(self.screen_buf, "{}", i)?;
-                    self.screen_buf.push_str("] ");
-                    let (nspaces, trimmed) =
+                    let (padding, trimmed) =
                         remove_common_prefix(std::mem::replace(&mut prevfile, file), file);
-                    self.screen_buf.extend(std::iter::repeat('.').take(nspaces));
-                    write!(self.screen_buf, "{}", trimmed)?;
-                    self.screen_buf.truncate(target_len);
-                    let nspaces = target_len - self.screen_buf.len();
-                    self.screen_buf.extend(std::iter::repeat(' ').take(nspaces));
+                    write!(
+                        self.screen_buf,
+                        "{:<rwidth$.rwidth$}",
+                        format!(
+                            " [{i:>iw$}] {pp:.<padding$}{trimmed}",
+                            iw = self.file_index_width as usize,
+                            pp = ""
+                        )
+                    )?;
                 }
-                None => self
-                    .screen_buf
-                    .extend(std::iter::repeat(' ').take(rwidth.saturating_sub(1))),
+                None => self.screen_buf.extend(std::iter::repeat(' ').take(rwidth)),
             }
         }
+        // Render the echo area.
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}├")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}├", "")?,
+        }
+        write!(self.screen_buf, "{:─<rwidth$.rwidth$}", "")?;
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}│")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}│", "")?,
+        }
+        write!(self.screen_buf, "{:<rwidth$.rwidth$}", self.session.echo())?;
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}├")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}├", "")?,
+        }
+        write!(self.screen_buf, "{:─<rwidth$.rwidth$}", "")?;
+        match tags.next() {
+            Some(tag) => write!(self.screen_buf, "{tag:<lwidth$.lwidth$}│")?,
+            None => write!(self.screen_buf, "{:<lwidth$.lwidth$}│", "")?,
+        }
+        write!(
+            self.screen_buf,
+            "{:<.rwidth$}",
+            format!(">>> {}", self.session.command())
+        )?;
         // Write the screen buffer out to the terminal in a single sys call.
         execute!(
             stdout,
@@ -291,8 +298,10 @@ fn run_app(stdout: &mut std::io::Stdout, app: &mut TuiApp) -> Result<(), TuiErro
     app.render(stdout)?;
     loop {
         // Poll events to see if redraw needed.
-        if let event::Event::Key(key) = crossterm::event::read()? {
-            app.keyevent(key);
+        if event::poll(std::time::Duration::from_millis(DELAY))? {
+            if let event::Event::Key(key) = crossterm::event::read()? {
+                app.keyevent(key);
+            }
             app.render(stdout)?;
         }
         if let State::Exit = app.session.state() {
@@ -301,112 +310,3 @@ fn run_app(stdout: &mut std::io::Stdout, app: &mut TuiApp) -> Result<(), TuiErro
     }
     Ok(())
 }
-
-// fn render_old(f: &mut Frame, app: &mut TuiApp) {
-//     const TAGWIDTH_PERCENT: u16 = 20;
-//     app.frameheight = f.area().height as usize;
-//     let hlayout = Layout::default()
-//         .direction(Direction::Horizontal)
-//         .constraints(vec![
-//             Constraint::Percentage(TAGWIDTH_PERCENT),
-//             Constraint::Percentage(100 - TAGWIDTH_PERCENT),
-//         ])
-//         .split(f.area());
-//     let rblocks = Layout::default()
-//         .direction(Direction::Vertical)
-//         .constraints(vec![
-//             Constraint::Max(1001),
-//             Constraint::Min(4),
-//             Constraint::Length(2),
-//         ])
-//         .split(hlayout[1]);
-//     let lblocks = Layout::default()
-//         .direction(Direction::Vertical)
-//         .constraints(vec![Constraint::Max(1000), Constraint::Length(6)])
-//         .split(hlayout[0]);
-//     let tagblock = lblocks[0];
-//     let filterblock = lblocks[1];
-//     let fileblock = rblocks[0];
-//     let echoblock = rblocks[1];
-//     let cmdblock = rblocks[2];
-//     // Tags.
-//     f.render_widget(
-//         Paragraph::new(
-//             app.session
-//                 .taglist()
-//                 .iter()
-//                 .map(|t| Line::from(t.clone()))
-//                 .collect::<Vec<_>>(),
-//         )
-//         .block(
-//             Block::new()
-//                 .borders(Borders::TOP | Borders::RIGHT)
-//                 .padding(Padding::horizontal(4)),
-//         )
-//         .scroll((app.scroll as u16, 0)),
-//         tagblock,
-//     );
-//     // Scroll bar.
-//     f.render_stateful_widget(
-//         Scrollbar::default()
-//             .orientation(ScrollbarOrientation::VerticalLeft)
-//             .begin_symbol(Some("↑"))
-//             .end_symbol(Some("↓")),
-//         tagblock,
-//         &mut app.scrollstate,
-//     );
-//     {
-//         let mut prevfile: &str = "";
-//         f.render_widget(
-//             Paragraph::new(
-//                 app.session
-//                     .filelist()
-//                     .iter()
-//                     .enumerate()
-//                     .map(|(filecounter, file)| {
-//                         let out = format!(
-//                             "[{}] {}",
-//                             {
-//                                 let nspaces = app.file_index_width - count_digits(filecounter);
-//                                 format!("{}{filecounter}", " ".repeat(nspaces as usize))
-//                             },
-//                             {
-//                                 let (space, trimmed) = remove_common_prefix(prevfile, file);
-//                                 format!("{}{}", ".".repeat(space), trimmed)
-//                             }
-//                         );
-//                         prevfile = file;
-//                         Line::from(out)
-//                     })
-//                     .collect::<Vec<_>>(),
-//             )
-//             .block(
-//                 Block::new()
-//                     .borders(Borders::TOP)
-//                     .padding(Padding::horizontal(2)),
-//             ),
-//             fileblock,
-//         );
-//     }
-//     f.render_widget(
-//         Paragraph::new(Text::from(app.session.echo())).block(
-//             Block::new()
-//                 .padding(Padding::horizontal(2))
-//                 .borders(Borders::TOP),
-//         ),
-//         echoblock,
-//     );
-//     f.render_widget(
-//         Paragraph::new(Text::from(app.session.filter_str())).block(
-//             Block::new()
-//                 .padding(Padding::horizontal(2))
-//                 .borders(Borders::TOP | Borders::RIGHT),
-//         ),
-//         filterblock,
-//     );
-//     f.render_widget(
-//         Paragraph::new(Text::from(format!(">>> {}█", app.session.command())))
-//             .block(Block::new().borders(Borders::TOP)),
-//         cmdblock,
-//     );
-// }
